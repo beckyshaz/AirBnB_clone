37cd5767d2d77924497c1ce53b8809c941aa1b67
beckyshaz <sharonsakah8@gmail.com>
2024-05-17 07:20:22 -0700

    kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -8,10 +8,28 @@ from datetime import datetime
 
 
 class BaseModel:
+    """The base class to inherit from """
     def __init__(self, *args, **kwargs):
+        """Initializing the instance attributes"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        if kwargs:
+            for key, val in kwargs.items():
+                if key == "__class__":
+                    del[key]
+                elif key == "created_at" or key == "updated_at":
+                    datetime_obj = datetime.strptime(
+                        val, "%Y-%m-%dT%H:%M:%S.%f"
+                    )
+                    setattr(self, key, datetime_obj)
+                else:
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         class_name = self.__class__.__name__
