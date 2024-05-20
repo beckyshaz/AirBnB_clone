1dd51549e2ca759a39511aa99e3a4fa46a25e96d
beckyshaz <sharonsakah8@gmail.com>
2024-05-20 06:49:13 -0700

    pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,15 +8,17 @@ import json
 
 class FileStorage:
     """Defining storage class"""
-    classes = {"BaseModel" : "BaseModel"}
+    classes = {"BaseModel": "BaseModel"}
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         return self.__objects
+
     def new(self, obj):
         keys = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[keys] = obj
+
     def save(self):
 
         dict_obj = {}
@@ -25,9 +27,10 @@ class FileStorage:
             dict_obj[key] = value.to_dict
             with open("file.json", "w", encoding="utf-8") as jsonFile:
                 json.dump(dict_obj, jsonFile)
+
     def reload(self):
         try:
-            with open("file.json", "r", encoding="utf-8") as jsonload:
+            with open("jsonFile", "r", encoding="utf-8") as jsonload:
                 deserialize = json.load(jsonload)
                 for key, value in deserialize.items():
                     class_name = v['__class__']
@@ -36,4 +39,3 @@ class FileStorage:
                         self.__objects[key] = instance
         except FileNotFoundError:
             pass
-
